4016ec22035302319b1e989eb162d617f3c50dfc
marcinlewinski <marcinlewinski71@gmail.com>
2023-07-12 14:07:22 +0200

    init project

---
+++ wild-event-manager-application/src/App.js
@@ -0,0 +1,10 @@
+import TestComponent from "./TestComponent";
+
+
+function App() {
+  return (
+   <TestComponent/>
+  );
+}
+
+export default App;
---
+++ wild-event-manager-application/src/TestComponent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TestComponent() {
+  return (
+    <div>
+      <h2>Hello World</h2>
+    </div>
+  )
+}
