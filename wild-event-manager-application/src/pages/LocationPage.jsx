535f9cf96bdf8b0eade311454b7226354e28c5fe
apoko <apokora@itsap.pl>
2023-07-27 13:27:09 +0200

    WILD-2 Add service on the frontend of location by id

--- wild-event-manager-application/src/pages/LocationPage.jsx
+++ wild-event-manager-application/src/pages/LocationPage.jsx
@@ -1,12 +1,30 @@
-import React from "react";
-import Location from '../components/location/Location'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import fetchLocationById from "../services/LocationService";
+import LoadingPage from "../components/LoadingPage";
+import Location from "../components/location/Location";
 
 const LocationPage = () => {
-    return (
-        <div>
-            <Location />
-        </div>
-    )
-}
+	const { id } = useParams()
+	const [location, setLocation] = useState(null)
+	const [loading, setLoading] = useState(true)
 
-export default LocationPage;
+	useEffect(() => {
+		setLoading(true)
+		fetchLocationById(id).then(locationData => {
+			setLocation(locationData)
+			setLoading(false)
+		})
+        .catch((error) => {
+            console.error("Error fetching location:", error);
+            setLoading(false);
+          });
+	}, [id])
+
+	if (loading) {
+		return <LoadingPage />
+	}
+
+	return <Location title={location.title} description={location.description} />
+}
+export default LocationPage
--- wild-event-manager-application/src/services/LocationService.js
+++ wild-event-manager-application/src/services/LocationService.js
@@ -1,1 +1,17 @@
-//todo
+const API_URL = "http://localhost:8080"
+
+const fetchLocationById = async id => {
+	
+	try {
+		const response = await fetch(`${API_URL}/location/${id}`)
+		if (!response.ok) {
+			throw new Error("Failed to fetch location")
+		}
+    return await response.json();
+	} catch (error) {
+		console.error("Error fetching location:", error)
+		throw error
+	}
+}
+
+export default fetchLocationById;
