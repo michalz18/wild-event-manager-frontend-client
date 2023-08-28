981f2429775a7be9d24b53a1bd81eb231832a5d6
apoko <apokora@itsap.pl>
2023-08-28 14:51:07 +0200

    WILD-67 Created card component for the event

--- wild-event-manager-application/src/components/event/Event.jsx
+++ wild-event-manager-application/src/components/event/Event.jsx
@@ -1,13 +1,21 @@
-const Event = ({ event }) => {
+import "./Event.scss"
 
-    return (
-        <div>
-            <h2>Title: {event.title}</h2>
-            <p>Date:{event.startsAt}  to: {event.endsAt}</p>
-            <p>Description: {event.description} </p>
-            <p>Location: {event.location} </p>
-        </div>
-    )
+const Event = ({ event }) => {
+	return (
+		<div className='card text-center m-5'>
+			<div className='card-header d-flex justify-content-center'>
+				<h2>{event.title}</h2>
+			</div>
+			<div className='card-body'>
+				<h3>Location: {event.location}</h3>
+				<p>{event.description}</p>
+			</div>
+			<div className='card-footer text-body-secondary mt-2'>
+				<p className="data-text">Start: {event.startsAt}</p>
+				<p>to: {event.endsAt}</p>
+			</div>
+		</div>
+	)
 }
 
-export default Event;
+export default Event
