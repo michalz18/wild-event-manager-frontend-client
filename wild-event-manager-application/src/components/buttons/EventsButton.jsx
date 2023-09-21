5151ad55ac966cacb5f080c32ce56cd37fdcb755
apoko <apokora@itsap.pl>
2023-09-21 18:49:26 +0200

    WILD-78 added style for events button

--- wild-event-manager-application/src/components/buttons/EventsButton.jsx
+++ wild-event-manager-application/src/components/buttons/EventsButton.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import "./EventsButton.scss";
 
 const EventsButton = () => {
     return (
         <Link to={'/events'}>
-        <button type="button" style={{ zIndex: 4, position: 'absolute' }}>
-            This is Events Button
+        <button className="EventsButton" type="button" style={{ zIndex: 4, position: 'absolute' }}>
+            Today's Events
         </button>
         </Link>
     )
